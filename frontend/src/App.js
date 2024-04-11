9f52933acf395e2be621db5062d40dcb890a6822
paliwal26 <dhruvpaliwal9690@gmail.com>
2024-04-11 11:30:03 +0530

    blog and event

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,30 @@
 
 import './App.css';
-import Footer from './Components/Footer';
-import Home from './Components/Home';
+import { Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar';
+import Home from './Components/Home';
+import Blogs from './Components/Blogs';
+import BlogsPage from './Components/BlogsPage';
+import NewsEvent from './Components/NewsEvent';
+import Footer from './Components/Footer';
+import NewsEventpage from './Components/NewsEventpage';
+
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Home/>
-      <Footer/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Blogs" element={<Blogs/>} />
+        <Route path="/BlogsPage" element={<BlogsPage />} />
+        <Route path="/NewsEvent" element={<NewsEvent/>} />
+        <Route path="/NewsEventpage" element={<NewsEventpage />} />
+        
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
