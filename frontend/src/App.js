1259c0f9b83b0576d2175ca40c7fc579fbd87df9
satyen mori <satyenmori@gmail.com>
2024-04-11 17:19:23 +0530

    Product Page UI made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,13 @@
-
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Navbar from './Components/Navbar';
-import Home from './Components/Home';
-import Blogs from './Components/Blogs';
-import BlogsPage from './Components/BlogsPage';
-import NewsEvent from './Components/NewsEvent';
-import Footer from './Components/Footer';
-import NewsEventpage from './Components/NewsEventpage';
-
-
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Blogs from "./Components/Blogs";
+import BlogsPage from "./Components/BlogsPage";
+import NewsEvent from "./Components/NewsEvent";
+import Footer from "./Components/Footer";
+import NewsEventpage from "./Components/NewsEventpage";
+import Product from "./Components/Product";
 
 function App() {
   return (
@@ -17,11 +15,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Blogs" element={<Blogs/>} />
+        <Route path="/Blogs" element={<Blogs />} />
         <Route path="/BlogsPage" element={<BlogsPage />} />
-        <Route path="/NewsEvent" element={<NewsEvent/>} />
+        <Route path="/NewsEvent" element={<NewsEvent />} />
         <Route path="/NewsEventpage" element={<NewsEventpage />} />
-        
+        <Route path="/Product" element={<Product />} />
       </Routes>
 
       <Footer />
---
+++ frontend/src/Components/Product.jsx
@@ -0,0 +1,188 @@
+import React from "react";
+import "../Style/Product.css";
+import blogbannerimage from "../images/blogbannerimage.svg";
+import rectangale159 from "../images/Rectangle159.svg";
+import rectangale160 from "../images/Rectangle160.svg";
+import rectangale161 from "../images/Rectangle161.svg";
+
+const Product = () => {
+  return (
+    <>
+      <section>
+        <div className="wavebgbanner">
+          <div className="main-width">
+            <div>
+              <div className="under1320width">
+                <div style={{ padding: "50px 0" }}>
+                  <div className="d-flex">
+                    <div className=" pure">
+                      <div className="blogsbannerpera">
+                        <p>Product</p>
+                      </div>
+                    </div>
+                    <div className="blogbannerimage">
+                      <img src={blogbannerimage} alt="" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section style={{ margin: "50px 0" }}>
+        <div className="main-width">
+          <div >
+            <div className="under1320width">
+              <div className="product-flex">
+                <div className="product-selecter">
+                  <div className="rectengle-one">
+                    <p>OUR PRODUCTS</p>
+                  </div>
+                  <div className="rectengle-all ">
+                    <p>BRANDED SYSTEM</p>
+                  </div>
+                  <div className="rectengle-all ">
+                    <p>PUMP</p>
+                  </div>
+                  <div className="rectengle-all ">
+                    <p>MEMBRANE</p>
+                  </div>
+                  <div className="rectengle-all ">
+                    <p>INLINE FILTERS</p>
+                  </div>
+                  <div className="rectengle-all ">
+                    <p>RO SMPS</p>
+                  </div>
+                  <div className="rectengle-all ">
+                    <p>MEMBRANE HOUSING</p>
+                  </div>
+                  <div className="rectengle-all ">
+                    <p>PP FILTER</p>
+                  </div>
+                  <div className="rectengle-last">
+                    <p>PP SPUN CARTRIDGE</p>
+                  </div>
+                </div>
+                <div className="product-card">
+                  <div className="card-flex">
+                    <div className="card">
+                      <img src={rectangale159} alt="rectangle159" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale160} alt="rectangle160" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale161} alt="rectangle161" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div><div className="card">
+                      <img src={rectangale159} alt="rectangle159" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale160} alt="rectangle160" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale161} alt="rectangle161" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale159} alt="rectangle159" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale160} alt="rectangle160" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale161} alt="rectangle161" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale159} alt="rectangle159" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale160} alt="rectangle160" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale161} alt="rectangle161" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale159} alt="rectangle159" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale160} alt="rectangle160" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale161} alt="rectangle161" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale159} alt="rectangle159" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale160} alt="rectangle160" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale161} alt="rectangle161" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale159} alt="rectangle159" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale160} alt="rectangle160" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale161} alt="rectangle161" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale159} alt="rectangle159" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale160} alt="rectangle160" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale161} alt="rectangle161" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale159} alt="rectangle159" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale160} alt="rectangle160" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                    <div className="card">
+                      <img src={rectangale161} alt="rectangle161" />
+                      <p>Lorem Ipsum is simply dummy text of</p>
+                    </div>
+                  
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Product;
